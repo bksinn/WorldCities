c76ca7c0fcfc1fb50c8e2890ef65ae087b64ca42
Bharat Singh <bksingh@g.cofc.edu>
2021-11-27 19:59:59 -0500

    Add WorldCities.Test project

---
+++ WorldCities.Tests/CitiesController_Test.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldCities.Data;
+using WorldCities.Data.Models;
+using Xunit;
+
+namespace WorldCities.Tests
+{
+    public class CitiesController_Test
+    {
+        ///<summary>
+        ///Test the GetCity() method
+        /// </summary>
+        [Fact]
+        public async void GetCity()
+        {
+            #region Arrange
+            //todo: define the required assets
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "WorldCities")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Add(new City()
+                {
+                    Id = 1,
+                    CountryId = 1,
+                    Lat = 1,
+                    Lon = 1,
+                    Name = "TestCity1"
+                });
+                context.SaveChanges();
+            }
+
+            City city_existing = null;
+            City city_notExisting = null;
+            #endregion
+            #region Act
+            //todo: invoke the test
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new CitiesController(context);
+                city_existing = (await controller.GetCity(1)).Value;
+                city_notExisting = (await controller.GetCity(2)).Value;
+            }
+            #endregion
+            #region Assert
+            //todo: verify that conditions are met
+            Assert.NotNull(city_existing);
+            Assert.Null(city_notExisting);
+            #endregion
+        }
+    }
+}
